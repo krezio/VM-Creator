41c53d1f0e53133cd635766aa8e06e48102819fe
krezio <contact.krezio@gmail.com>
2023-07-29 04:41:14 +0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# VM-Creator
-This Discord bot allows you to create virtual machines (VMs) on Microsoft Azure using a simple command. It automates the process &amp; allows certain configurations.
+# Azure VM Creation Bot
+* This Discord bot allows you to create virtual machines (VMs) on Microsoft Azure using a simple command. It automates the process & allows certain configurations.
+
+___
+## Prerequisites
+Before using the bot, please make sure you have the following:
+
+- Azure subscription
+- Azure Subscription ID, Client ID, Client Secret, and Tenant ID.
+- Discord bot token
+- A brain
+
+___
+## Setup
+
+1. Clone the repo
+`git clone https://github.com/your-username/vm-creation-discord-bot.git`
+
+2. Install the required dependencies:
+* `cd vm-creation-discord-bot`
+* `pip install -r requirements.txt`
+* `pip install -r requirements.txt`
+
+3. Open the bot.py file and replace the placeholder values for Azure credentials and Discord Bot Token with your own credentials.
+
+4. Run the bot through `python bot.py`
+
+___
+## Usage
+The command format to create a VM is:
+**!createvm** `<username>` `<password>` `<ram>` `<cpu>` `<storage>`
+* `<username>`: The desired username for the VM.
+* `<password>`: The password for the administrator account.
+* `<ram>`: The RAM size in gigabytes for the VM.
+* `<cpu>`: The number of virtual CPUs (vCPUs) for the VM.
+* `<storage>`: The storage size in gigabytes for the VM.
+
+For example, to create a VM with 32GB RAM, 4 vCPUs, and 128GB storage, you would use the following command:
+- `!createvm johnsmith P@ssw0rd123 32 4 128`
+
+If your VM requires futher configurations, modification of the code will be required.
---
+++ bot.py
@@ -0,0 +1,160 @@
+import os
+import random
+import string
+from azure.common.credentials import ServicePrincipalCredentials
+from azure.mgmt.compute import ComputeManagementClient
+from azure.mgmt.compute.models import NetworkProfile, NetworkInterfaceReference, OSProfile, \
+    HardwareProfile, StorageProfile, StorageAccountTypes, VirtualMachine, CachingTypes
+from msrestazure.azure_exceptions import CloudError
+import discord
+
+
+# Discord Bot Token
+DISCORD_TOKEN = 'tokenhere'
+
+# Azure Credentials
+SUBSCRIPTION_ID = 'YOUR_SUBSCRIPTION_ID'
+CLIENT_ID = 'YOUR_AZURE_CLIENT_ID'
+CLIENT_SECRET = 'YOUR_AZURE_CLIENT_SECRET'
+TENANT_ID = 'YOUR_AZURE_TENANT_ID'
+
+# Azure Configuration
+LOCATION = 'southindia'  # Azure region where the VM will be created
+
+
+# Generate a random string
+def generate_random_string(length):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for _ in range(length))
+
+
+# Create Azure Compute Management client
+def create_compute_client():
+    credentials = ServicePrincipalCredentials(
+        client_id=CLIENT_ID,
+        secret=CLIENT_SECRET,
+        tenant=TENANT_ID
+    )
+    return ComputeManagementClient(credentials, SUBSCRIPTION_ID)
+
+
+# Create virtual machine
+def create_virtual_machine(username, admin_password, ram_size, vcpu_size, storage_size):
+    compute_client = create_compute_client()
+
+    # Generate resource group name
+    resource_group = f'{username}-rg'
+
+    # Generate network interface name
+    network_interface_name = f'{username}-nic'
+
+    # Generate virtual machine name
+    vm_name = f'{username}-vm'
+
+    # Create resource group
+    compute_client.resource_groups.create_or_update(resource_group, {'location': LOCATION})
+
+    # Create network interface
+    network_interface_params = {
+        'location': LOCATION,
+        'ip_configurations': [{
+            'name': 'MyIPConfig',
+            'subnet': {'id': f'/subscriptions/{SUBSCRIPTION_ID}/resourceGroups/{resource_group}/providers/Microsoft.Network/virtualNetworks/{resource_group}-vnet/subnets/default'}
+        }]
+    }
+    network_interface = compute_client.network_interfaces.create_or_update(resource_group, network_interface_name, network_interface_params).result()
+
+    # Create virtual machine
+    vm_params = {
+        'location': LOCATION,
+        'os_profile': OSProfile(
+            computer_name=vm_name,
+            admin_username=username,
+            admin_password=admin_password,
+            windows_configuration={
+                'enable_automatic_updates': True,
+                'patch_settings': {'patch_mode': 'AutomaticByOS'},
+                'additional_unattend_content': [{
+                    'pass': 'oobesystem',
+                    'component': 'Microsoft-Windows-Shell-Setup',
+                    'setting_name': 'FirstLogonCommands',
+                    'content': '<FirstLogonCommands>\n' +
+                               '  <SynchronousCommand wcm:action="add">\n' +
+                               '    <CommandLine>cmd.exe /c netsh advfirewall firewall add rule name="RDP" dir=in action=allow protocol=TCP localport=3389</CommandLine>\n' +
+                               '    <Description>Open RDP Port</Description>\n' +
+                               '    <Order>1</Order>\n' +
+                               '    <RequiresUserInput>false</RequiresUserInput>\n' +
+                               '  </SynchronousCommand>\n' +
+                               '</FirstLogonCommands>'
+                }]
+            }
+        ),
+        'hardware_profile': HardwareProfile(vm_size=f'Standard_{vcpu_size}vCPUs_{ram_size}GB'),
+        'storage_profile': StorageProfile(
+            image_reference={
+                'publisher': 'MicrosoftWindowsServer',
+                'offer': 'WindowsServer',
+                'sku': '2022-datacenter',
+                'version': 'latest'
+            },
+            os_disk={
+                'create_option': 'FromImage',
+                'caching': CachingTypes.none,
+                'managed_disk': {
+                    'storage_account_type': StorageAccountTypes.standard_lrs
+                },
+                'disk_size_gb': storage_size
+            }
+        ),
+        'network_profile': NetworkProfile(
+            network_interfaces=[NetworkInterfaceReference(id=network_interface.id)]
+        )
+    }
+
+    try:
+        compute_client.virtual_machines.create_or_update(resource_group, vm_name, VirtualMachine(vm_params))
+        return True
+    except CloudError as e:
+        print(f'Error creating virtual machine: {e}')
+        return False
+
+
+
+
+
+# Create Discord client
+bot = discord.Client()
+
+
+# Event: Bot is ready
+@bot.event
+async def on_ready():
+    print('Bot is ready!')
+
+
+# Event: Bot receives a message
+@bot.event
+async def on_message(message):
+    if message.content.startswith('!createvm'):
+        args = message.content.split()
+
+        if len(args) != 6:
+            await message.reply('Invalid command format. Usage: !createvm <username> <password> <ram_size> <vcpu_size> <storage_size>')
+            return
+
+        username = args[1]
+        admin_password = args[2]
+        ram_size = args[3]
+        vcpu_size = args[4]
+        storage_size = args[5]
+
+        result = create_virtual_machine(username, admin_password, ram_size, vcpu_size, storage_size)
+
+        if result:
+            await message.reply(f'Virtual machine for {username} created successfully!')
+        else:
+            await message.reply(f'Error creating virtual machine for {username}.')
+
+
+# Run the bot
+bot.run(DISCORD_TOKEN)
